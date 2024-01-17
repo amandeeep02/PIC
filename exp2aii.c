e703852bc9a34e0dccce8a7b8508cf75c8e8c316
amandeeep02 <amandeepsr02@gmail.com>
2024-01-18 00:32:11 +0530

    Used nexted if else

--- exp2aii.c
+++ exp2aii.c
@@ -3,19 +3,21 @@ int main()
 {
     int a,b,c;
     printf("Name: Amandeep Singh\nRoll No: 16010123036\n");
-    printf("Enter Three Integers: ");
+    printf("Enter Three Integers:\n");
     scanf("%d %d %d",&a,&b,&c);
     int max=0;
-    if(a>b&&a>c)
+    if(a>b)
     {
+        if(a>c)
         max=a;
-    }
-    else if (b>a&&b>c)
-    {
-        max=b;
+        else
+        max=c;
     }
     else
     {
+        if(b>c)
+        max=b;
+        else
         max=c;
     }
     printf("The maximum value is (using If -Else if-Else): %d\n",max);
